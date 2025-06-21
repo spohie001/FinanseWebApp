64bcc66862c5ae217342278397381d7d0218a3b0
spohie001 <z.sosinska001@gmail.com>
2025-06-21 19:26:29 +0200

    setting up SQL Server with EntityFramework

---
+++ FinanseWebApp/Data/FinanseAppCtx.cs
@@ -0,0 +1,13 @@
+﻿using FinanseWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanseWebApp.Data
+{
+    public class FinanseAppCtx : DbContext
+    {
+        public FinanseAppCtx(DbContextOptions<FinanseAppCtx> options) : base(options) { }   
+        
+        DbSet<Expense> Expenses { get; set; }
+        
+    }
+}
--- FinanseWebApp/Program.cs
+++ FinanseWebApp/Program.cs
@@ -1,7 +1,11 @@
+using FinanseWebApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<FinanseAppCtx>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
